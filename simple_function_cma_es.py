c1991d6fceccb682241b357d1ae470b31505ff42
NicolaVisentin <nicvise2001@gmail.com>
2025-10-23 21:57:10 +0200

    Added utilis.py script and completed simple customized CMA optimization

---
+++ simple_function_cma_es.py
@@ -0,0 +1,191 @@
+# =====================================================
+# Setup
+# =====================================================
+
+# Choose device (cpu or gpu)
+import os
+os.environ["JAX_PLATFORM_NAME"] = "cpu"
+
+# Imports
+import jax
+import jax.numpy as jnp
+from pathlib import Path
+import matplotlib.pyplot as plt
+import pickle
+
+from evosax.algorithms.distribution_based.cma_es import CMA_ES
+from utilis import *
+
+# Set random seed
+seed = 123
+key = jax.random.key(seed)
+
+# jax settings
+jax.config.update("jax_enable_x64", True)  # double precision
+jnp.set_printoptions(
+    threshold=jnp.inf,
+    linewidth=jnp.inf,
+    formatter={"float_kind": lambda x: "0" if x == 0 else f"{x:.2e}"},
+)
+
+# Folder for plots and videos
+curr_folder = Path(__file__).parent
+plots_folder = curr_folder/'plots and videos'/Path(__file__).stem
+plots_folder.mkdir(parents=True, exist_ok=True)
+
+# Folder for saving data
+data_folder = curr_folder/'saved data'/Path(__file__).stem
+data_folder.mkdir(parents=True, exist_ok=True)
+
+
+# =====================================================
+# Define the problem (function to optimize)
+# =====================================================
+    
+# Set cost function
+@jax.jit
+def cost(x):
+    J = jnp.sin(0.05*x[0]) * jnp.cos(0.05*x[1]) + jnp.sin(0.02*x[0]) * jnp.cos(0.02*x[1])
+    return J
+
+# Set research bounds
+min_vals = jnp.array([-70, -80])
+max_vals = jnp.array([70, 60])
+
+# Instantiate the problem
+problem = MyOptimizationProblem(
+    cost_fn=cost,
+    lower_research_bound=min_vals,
+    upper_research_bound=max_vals,
+)
+
+
+# =====================================================
+# Define the optimization strategy (CMA-ES)
+# =====================================================
+
+# Set parameters
+num_generations = 24  # number of iterations
+population_size = 128 # dimension of the population
+
+# Set info to save during the iterations
+def metrics_fn(key, population, fitness, state, params):
+    idx = jnp.argmin(fitness)
+    metrics = {
+        "fitness": fitness,           # fitness of all individuals
+        "best_fitness": fitness[idx], # best fitness among all individuals
+        "mean": state.mean,           # mean of the distribution (solution)
+        "populations": population     # population
+    }
+    return metrics
+
+# Instantiate the search strategy and initialize a solution and state of the optimizer
+key, subkey = jax.random.split(key)
+#solution0 = problem.sample(subkey) # initial point (mean) for the research
+solution0 = jnp.array([20.0, -20.0])
+
+cma = CMA_ES(
+    population_size=population_size, 
+    solution=solution0,
+    metrics_fn=metrics_fn,
+)
+params = cma.default_params # parameters for CMA-ES (e.g. learning rate for the mean, learning rate for the covariance...)
+
+key, subkey = jax.random.split(key)
+state0 = cma.init(subkey, solution0, params) # e.g. current best solution, current best fitness, current generation counter...
+
+
+# =====================================================
+# Run the optimization
+# =====================================================
+
+# Single iteration step (for jax.lax.scan)
+def step(carry, key):
+    # extract CMA-ES params and current CMA-ES state from carry
+    state, params = carry
+    key_ask, key_tell = jax.random.split(key, 2)
+
+    # sample a population basing on the current state
+    population, state = cma.ask(key_ask, state, params)
+    population = jnp.clip(population, min_vals, max_vals) # clip population within the bounds
+    
+    # evaluate the new population (return fitness) and update the counter
+    fitness = problem.eval(population)
+
+    # update the state basing on the evaluation of the new population
+    state, metrics = cma.tell(key_tell, population, fitness, state, params)
+
+    carry = (state, params)
+    out = metrics
+    return carry, out
+
+# Execute the iterations
+key, subkey = jax.random.split(key)
+keys = jax.random.split(subkey, num_generations)
+carry0 = (state0, params)
+_, metrics = jax.lax.scan(
+    step,
+    carry0,
+    keys,
+)
+
+# Extract results
+means = metrics["mean"]                  # mean of the population. Shape (num_generations, num_dims)
+fitness_means = problem.eval(means)      # fitness of the mean. Shape (num_generations,)
+populations = metrics["populations"]     # population. Shape (num_generations, population_size, num_dims)
+fitness_populations = metrics["fitness"] # fitness of all individuals. Shape (num_generations, population_size)
+best_fitness = metrics["best_fitness"]   # fitness of the best individual. Shape (num_generations,)
+
+
+# =====================================================
+# Visualize results
+# =====================================================
+
+# Plot best fitness for each generation vs generations
+plt.figure()
+plt.plot(fitness_means, label='fitness of the solution')
+plt.plot(best_fitness, label='best fitness in population')
+plt.grid(True)
+plt.xlabel('generation')
+plt.ylabel('fitness')
+plt.title('Fitness curve')
+plt.legend()
+plt.tight_layout()
+plt.savefig(plots_folder/'fitness curve', bbox_inches='tight')
+#plt.show()
+
+# Plot 3D
+x = np.linspace(problem.lower_research_bound[0], problem.upper_research_bound[0], 101)
+y = np.linspace(problem.lower_research_bound[1], problem.upper_research_bound[1], 101)
+X, Y = np.meshgrid(x, y)
+points = np.stack([X.flatten(), Y.flatten()], axis=1)
+Z = problem.eval(points).reshape(X.shape)
+
+fig = plt.figure(figsize=(8, 8))
+ax = fig.add_subplot(111, projection='3d')
+surf = ax.plot_surface(X, Y, Z, cmap='viridis', alpha=0.75, zorder=1)
+sol = ax.scatter(means[-1,0], means[-1,1], fitness_means[-1], color='r', s=30, zorder=2, label='solution')
+cbar = fig.colorbar(surf, ax=ax, shrink=0.6)
+cbar.set_label('fitness')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('fitness')
+ax.set_xlim([problem.lower_research_bound[0], problem.upper_research_bound[0]])
+ax.set_ylim([problem.lower_research_bound[1], problem.upper_research_bound[1]])
+ax.legend()
+plt.tight_layout()
+plt.savefig(plots_folder/'solution', bbox_inches='tight')
+with open(plots_folder/'solution', 'wb') as f:
+          pickle.dump(plt.gcf(), f)
+plt.show()
+
+# Visualize and save the optimization animation
+animate_evolution(
+    problem=problem,
+    means=means,
+    populations=populations,
+    duration=5,
+    grid_res=101,
+    savepath=plots_folder/'animation.gif',
+    show=True,
+)
---
+++ utilis.py
@@ -0,0 +1,234 @@
+import numpy as np
+import jax
+from jax import Array
+from functools import partial
+from typing import Callable
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation, PillowWriter
+
+# =====================================================
+# Class for optimization problems to run with evosax package
+# =====================================================
+class MyOptimizationProblem:
+    """
+    Class for defining custom optimization problem to use with evosax.
+
+    Parameters
+    ----------
+    cost_fn : Callable
+        Cost function J = f(x), with x n-dimensional array.
+    lower_research_bound : Array
+        Lower bound for each research parameter (x1, x2, ..., xn). Shape (n,)
+    upper_research_bound : Array
+        Upper bound for each research parameter (x1, x2, ..., xn). Shape (n,)
+
+    Attributes
+    ----------
+    cost_fn : Callable
+        Cost function J = f(x), with x n-dimensional array.
+    lower_research_bound : Array
+        Lower bound for each research parameter (x1, x2, ..., xn). Shape (n,)
+    upper_research_bound : Array
+        Upper bound for each research parameter (x1, x2, ..., xn). Shape (n,)
+    dim : int
+        Dimension of the problem (n).
+    """
+    def __init__(
+            self, 
+            cost_fn: Callable,
+            lower_research_bound: Array, 
+            upper_research_bound: Array,
+    ):
+        if len(lower_research_bound) != len(upper_research_bound):
+            raise ValueError('lower_research_bound and upper_research_bound must have same length.')
+        self.lower_research_bound = lower_research_bound
+        self.upper_research_bound = upper_research_bound
+        self.dim = len(self.lower_research_bound)
+        self.cost_fn = jax.vmap(cost_fn, in_axes=0) # vmap cost function to accept batch of inputs
+
+    @partial(jax.jit, static_argnames=("self",))
+    def eval(self, points_batch: Array) -> Array:
+        """
+        Evaluates J over a batch of points x.
+
+        Args
+        ----
+        points_batch : Array
+            Batch of points. Shape (batch_size, n)
+        
+        Returns
+        -------
+        fitness_batch : Array
+            Value of J for the given points. Shape (batch_size,)
+        """
+        if len(points_batch.shape) == 1:
+            points_batch = points_batch[None,:]
+        fitness_batch = self.cost_fn(points_batch)
+        return fitness_batch
+
+    @partial(jax.jit, static_argnames=("self",))
+    def sample(self, key) -> Array:
+        """
+        Samples a random point x in the research space.
+        
+        Args
+        ----
+        key : Array
+            Random key.
+        
+        Returns
+        -------
+        point : Array
+            Random point within the research bounds. Shape (n,)
+        """
+        point = jax.random.uniform(
+            key=key, 
+            shape=(self.dim,), 
+            minval=self.lower_research_bound, 
+            maxval=self.upper_research_bound
+        )
+        return point
+    
+# =====================================================
+# Function to animate the progress of the optimization (! ONLY FOR 2D CASES)
+# =====================================================
+def animate_evolution(
+        problem,
+        means : Array,
+        populations : Array = None,
+        duration: int = 10,
+        grid_res: int = 0,
+        savepath = None,
+        show: bool = True
+):    
+    """
+    Function to animate evolution of the evolutionary algorithm.
+
+    Args
+    ----
+    problem
+        Optimization problem defined by the user. It's a Problem object from evosax.
+    means : Array
+        Solution (mean of the population) for each iteration. Shape (num_generations, num_dims)
+    populations : Array, optional
+        Population for each iteration. Shape (num_generations, population_size, num_dims)
+    duration : int, optional
+        Duration of the animation in seconds (default: 10). 
+    grid_res : int, optional
+        Resolution of the grid for plotting the surface (default: 0). If zero, does not plot the surface.
+    savepath : optional
+        Path to save the animation, in form /my_path/'name_animation.gif'. Animation will be saved
+        in /my_path as 'name_amination.gif'. If None, animation will not be saved.
+    show : bool, optional
+        True to print the animation (default: True).
+    """
+    num_generations = len(means)
+
+    # compute fitness of the mean at each iteration. Shape (num_generations,)
+    fitness_means = problem.eval(means)
+    
+    # compute fitness of all individuals at each iteration. Shape (num_generations, population_size)
+    if populations is not None:
+        pop = populations
+        eval_pop = jax.vmap(problem.eval, in_axes=0)
+        fitness_populations = eval_pop(pop)
+    else:
+        pop = np.full((num_generations,1,2), np.nan)
+        fitness_populations = np.full((num_generations,1), np.nan)
+    
+    # compute fitness surface in the research space
+    if grid_res != 0:
+        x = np.linspace(problem.lower_research_bound[0], problem.upper_research_bound[0], grid_res)
+        y = np.linspace(problem.lower_research_bound[1], problem.upper_research_bound[1], grid_res)
+        X, Y = np.meshgrid(x, y)
+        points = np.stack([X.flatten(), Y.flatten()], axis=1)
+        Z = problem.eval(points).reshape(X.shape)
+    else:
+        if populations is not None:
+            z_min = np.min([np.min(fitness_means), np.min(fitness_populations)])
+            z_max = np.max([np.max(fitness_means), np.max(fitness_populations)])
+        else:
+            z_min = np.min(fitness_means)
+            z_max = np.max(fitness_means)
+
+    # create figure
+    fig = plt.figure(figsize=(6, 10))
+
+    # add 3D plot
+    ax3D = fig.add_subplot(211, projection='3d')
+    if grid_res != 0:
+        ax3D.plot_surface(X, Y, Z, cmap='viridis', alpha=0.5, zorder=1)
+    else:
+        ax3D.set_zlim([z_min, z_max])
+    sol3D = ax3D.scatter([], [], [], color='r', s=20, zorder=3, label='mean')
+    pop3D = ax3D.scatter([], [], [], color='orange', s=20, zorder=2)
+    if populations is not None:
+        pop3D.set_label('population')
+    ax3D.set_xlabel('x')
+    ax3D.set_ylabel('y')
+    ax3D.set_zlabel('fitness')
+    title = ax3D.set_title("Generation: 0")
+    ax3D.set_xlim([problem.lower_research_bound[0], problem.upper_research_bound[0]])
+    ax3D.set_ylim([problem.lower_research_bound[1], problem.upper_research_bound[1]])
+    ax3D.legend()
+
+    # add 2D plot
+    ax2D = fig.add_subplot(212, )
+    if grid_res != 0:
+        plt2d = ax2D.contourf(X, Y, Z, cmap='viridis', zorder=1)
+        cbar = fig.colorbar(plt2d, ax=ax2D, shrink=1.0)
+        cbar.set_label('fitness')
+    sol2D = ax2D.scatter([], [], color='r', s=20, zorder=3, label='mean')
+    pop2D = ax2D.scatter([], [], color='orange', s=20, zorder=2)
+    if populations is not None:
+        pop2D.set_label('population')
+    ax2D.set_xlabel('x')
+    ax2D.set_ylabel('y')
+    ax2D.grid(True)
+    ax2D.set_xlim([problem.lower_research_bound[0], problem.upper_research_bound[0]])
+    ax2D.set_ylim([problem.lower_research_bound[1], problem.upper_research_bound[1]])
+    ax2D.legend()
+
+    def init():
+        sol3D._offsets3d = ([], [], [])
+        pop3D._offsets3d = ([], [], [])
+        sol2D.set_offsets(np.empty((0, 2)))
+        pop2D.set_offsets(np.empty((0, 2)))
+        title.set_text("Generation: 0")
+        return sol3D, pop3D, sol2D, pop2D, title
+
+    def update(frame):
+        x_mean = means[frame, 0]
+        y_mean = means[frame, 1]
+        z_mean = fitness_means[frame]
+        x_pop = pop[frame, :, 0]
+        y_pop = pop[frame, :, 1]
+        z_pop = fitness_populations[frame]
+
+        title.set_text(f"Generation: {frame}") 
+        sol3D._offsets3d = ([x_mean], [y_mean], [z_mean])
+        pop3D._offsets3d = (x_pop, y_pop, z_pop)
+        sol2D.set_offsets(np.column_stack((x_mean, y_mean)))
+        pop2D.set_offsets(np.column_stack((x_pop, y_pop)))
+
+        return sol3D, pop3D, sol2D, pop2D, title
+    
+    plt.tight_layout()
+
+    # generate animation
+    n_frames = len(means)
+    fps = n_frames / duration
+    ani = FuncAnimation(
+        fig, update, init_func=init,
+        frames=n_frames, interval=1000/fps, blit=False, repeat_delay=1500,
+    )
+
+    # save animation in specified path
+    if savepath is not None:
+        ani.save(savepath, writer=PillowWriter(fps=fps))
+
+    # show animation
+    if show:
+        plt.show()
+    else:
+        plt.close()
