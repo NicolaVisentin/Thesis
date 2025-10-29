21627a7f1e06538a9cad1f6252acac80b43ce81a
NicolaVisentin <nicvise2001@gmail.com>
2025-10-29 18:51:14 +0100

    Added function to generate indices for generating shuffled batches from a datasets

--- scripts/utilis.py
+++ scripts/utilis.py
@@ -312,3 +312,51 @@ def split_dataset(
     test_set  = {k: v[perm[test_end:]] for k, v in dataset.items()}
 
     return train_set, val_set, test_set
+
+
+# Shuffled batch generator
+@partial(jax.jit, static_argnums=(1,2))
+def batch_indx_generator(
+    key,
+    dataset_size : int, 
+    batch_size : int = 512,
+) -> Array:
+    """
+    Given a certain dataset_size and a certain desired batch_size, his function
+    returns the indices to generate a shuffled collection of batches out of a dataset. 
+    It is thought for working with lax.scan loops.
+
+    Args
+    ----
+    key
+        jax.random.key.
+    dataset_size : int
+        Dataset size.
+    batch_size : int
+        Batch size (default: 512).
+    
+    Returns
+    -------
+    batch_ids : Array
+        Array containing indices to extract shuffled batches out of a dataset. If dataset_size is 
+        not divisible by batch_size, then the last batch is filled with repeated data. Shape (n_batches, batch_size)
+
+    Example
+    -------
+    dataset_size=10, batch_size=3 -> batch_ids=[[0,7,3],[1,9,4],[2,6,8],[5,0,7]]
+    """
+    # Shuffle dataset indices
+    key, subkey = jax.random.split(key)
+    perm = jax.random.permutation(subkey, dataset_size)
+
+    # Number of batches (ceil division)
+    num_batches = (dataset_size + batch_size - 1) // batch_size
+
+    # Pad permutation to make reshape legal: extend last batch if incomplete
+    pad_size = num_batches * batch_size - dataset_size
+    perm = jnp.pad(perm, (0, pad_size), mode="wrap")
+
+    # Reshape into (num_batches, batch_size)
+    batch_ids = perm.reshape((num_batches, batch_size))
+
+    return batch_ids
