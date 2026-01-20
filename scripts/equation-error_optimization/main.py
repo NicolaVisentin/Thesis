622c93828b4c3ac0bfbabe6530ac259c2324ed0d
NicolaVisentin <nicvise2001@gmail.com>
2026-01-20 15:21:14 +0100

    prepared for workstation

--- scripts/equation-error_optimization/main.py
+++ scripts/equation-error_optimization/main.py
@@ -210,20 +210,20 @@ def animate_robot_matplotlib(
 
 # General
 load_experiment = False # choose whether to load saved experiment or to perform training
-experiment = 'T66' # name of the experiment to perform/load
+experiment = 'A1' # name of the experiment to perform/load
 use_scan = True # choose whether to use normal for loop or lax.scan
 show_simulations = True # choose whether to perform time simulations of the approximator (and comparison with RON)
 
 # Reference RON reservoir
 ron_case = 'input' # 'simple' 'coupled' 'input'
-ron_dataset = 'N6_withInput/dataset_m1e5_N6_withInput' # name of the case to load from 'soft robot optimization' folder
-ron_evolution_example = 'N6_withInput/RON_evolution_N6_withInput' # name of the case to load from 'soft robot optimization' folder
+ron_dataset = 'N6_DT0.006_RHO0.99/dataset_m1e5_N6_DT0.006_RHO0.99' # name of the case to load from 'soft robot optimization' folder
+ron_evolution_example = 'N6_DT0.006_RHO0.99/RON_evolution_N6_DT0.006_RHO0.99' # name of the case to load from 'soft robot optimization' folder
 
 # controller
 train_unique_controller = False # if True, tau = tau_tot(z, u), where tau_tot is specified in fb_controller_to_train. 
                                # If False, tau = tau_fb(z) + tau_ff(u), where tau_fb is specified in fb_controller_to_train and tau_ff in ff_controller_to_train
-fb_controller_to_train = 'linear_complete' # 'linear_simple', 'linear_complete', 'tanh_simple', 'tanh_complete', 'mlp'
-ff_controller_to_train = 'linear' # (only applies to train_unique_controller = False). Choose 'linear', 'tanh', 'mlp'
+fb_controller_to_train = 'mlp' # 'linear_simple', 'linear_complete', 'tanh_simple', 'tanh_complete', 'mlp'
+ff_controller_to_train = 'mlp' # (only applies to train_unique_controller = False). Choose 'linear', 'tanh', 'mlp'
 
 # Mapping
 map_to_train = 'svd' # 'diag', 'svd', 'reconstruction', 'norm_flow'
