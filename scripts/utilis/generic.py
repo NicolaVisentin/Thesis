08aee01663445b3b4178c1474b4ace7b94e0abbd
NicolaVisentin <nicvise2001@gmail.com>
2026-01-27 18:31:24 +0100

    Added function to load Mackey-Glass dataset.

--- scripts/utilis/generic.py
+++ scripts/utilis/generic.py
@@ -184,6 +184,45 @@ def extract_batch(
     return batch
 
 
+# Load Mackey-Glass dataset
+def load_mackey_glass_data(csvfolder: Path, lag=84, washout=200):
+    """Get the Mackey-Glass dataset and return the train, validation and test datasets
+    as numpy arrays.
+
+    Args:
+        csvfolder (Path): Path to the directory containing the mackey_glass.csv file.
+        lag (int, optional): Number of time steps to look back. Defaults to 84.
+        washout (int, optional): Number of time steps to discard. Defaults to 200.
+
+    Returns:
+        Tuple[Tuple[Array, Array], Tuple[Array, Array], Tuple[Array, Array]]: Train, validation and test datasets.
+    """
+    with open(csvfolder/'mackey_glass.csv', "r") as f:
+        data_lines = f.readlines()[0]
+
+    # 10k steps
+    dataset = np.array([float(el) for el in data_lines.split(",")], dtype=np.float64)
+
+    end_train = int(dataset.shape[0] / 2)
+    end_val = end_train + int(dataset.shape[0] / 4)
+    end_test = dataset.shape[0]
+
+    train_dataset = dataset[: end_train - lag]
+    train_target = dataset[washout + lag : end_train]
+
+    val_dataset = dataset[end_train : end_val - lag]
+    val_target = dataset[end_train + washout + lag : end_val]
+
+    test_dataset = dataset[end_val : end_test - lag]
+    test_target = dataset[end_val + washout + lag : end_test]
+
+    return (
+        (train_dataset, train_target),
+        (val_dataset, val_target),
+        (test_dataset, test_target),
+    )
+
+
 # =====================================================
 # Other
 # =====================================================
